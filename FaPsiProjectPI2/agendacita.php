827bc17b8428a884b4349dd9003386166dc8a9df
Gerardo Villarreal <Gerardo Villarreal@Sakkon>
2023-10-22 23:52:55 -0600

    Hugo's updates

--- FaPsiProjectPI2/agendacita.php
+++ FaPsiProjectPI2/agendacita.php
@@ -1,31 +1,25 @@
-<?php 
+<?php
 
 include("conexion.php");
 
+if (isset($_POST["submitagenda"])) {
 
+    $fecha = $_POST["fecha"];
+    $horas = $_POST["horas"];
+    $dia_semana = date('w', strtotime($fecha));
+    $dia_semana = intval($dia_semana);
+    $fechaInt = preg_replace('[\D]', '', $fecha);
 
-if(isset($_POST["submitagenda"])){
+    session_start();
+    $user = $_SESSION['dev'];
+    $id_paciente = $_SESSION['id_paciente'];
 
-$fecha = $_POST["fecha"];
-$horas = $_POST["horas"]; 
-$nombres = '';
-$correo = '';
-session_start();
-$user = $_SESSION['dev'];
-$id_paciente = $_SESSION['id_paciente'];
+    $sqlwrite = "INSERT INTO horarios_citas (id_paciente, id_hora, id_disponibilidad, int_fecha, id_dia) 
+    VALUES ('$id_paciente','$horas', 1, '$fechaInt', '$dia_semana')";
 
-$fechayhoras = $fecha.', '.$horas;
-$disp = 'Pendiente';
-
-$sqlwrite = "INSERT INTO `horarios`(`id_paciente`,`fechayhoras`,`isdisponible`) VALUES ('$id_paciente','$fechayhoras', '$disp')";
-
- if (mysqli_query($conn, $sqlwrite)) {
-        echo  '<script> alert("Datos insertados correctamente en la base de datos."); window.location = "AGENDA.php" </script>';
+    if (mysqli_query($conn, $sqlwrite)) {
+        echo  '<script> alert("Peticion de cita mandada correctamente. Espere a que sea aceptada por un profesional"); window.location = "AGENDA.php" </script>';
     } else {
         echo "Error al insertar los datos: " . mysqli_error($conn);
     }
-
-
 }
-
-?>
